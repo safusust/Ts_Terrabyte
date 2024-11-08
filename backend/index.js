2d21b19495f98d3d13c1ceac1c1bfff28432a00c
Md Yak Safu <safusust@gmail.com>
2024-11-08 10:39:26 +0600

    env

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,12 @@ const connectDB=require('./config/configDB')
 const app = express();
 dotenv.config();
 
-
-
 const PORT = 3000;
+
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.send("Welcome to root URL of Server");
+});
 
 app.listen(PORT, function(err){
     if (err) console.log("Error in server setup")
